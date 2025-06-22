eec9e764f12757050e6119d225efcad5d262e382
Jason Warner <xocomil@gmail.com>
2025-06-22 12:05:40 -0600

    Refactors hero name update to use event
    Updates the hero name update mechanism to dispatch an event, which is then handled by the reducer.
    
    This change improves the separation of concerns and promotes a more reactive approach to state management.

--- libs/ui/src/lib/stores/heroes.store.ts
+++ libs/ui/src/lib/stores/heroes.store.ts
@@ -1,18 +1,37 @@
-import { patchState, signalStore, withMethods, withState } from '@ngrx/signals';
+import {
+  PartialStateUpdater,
+  signalStore,
+  type,
+  withState,
+} from '@ngrx/signals';
+import { eventGroup, on, withReducer } from '@ngrx/signals/events';
 import { create } from 'mutative';
+import { Hero } from '../models/hero.model';
 import { createHeroesView } from '../models/heroes-view.model';
+
+export const heroesStateEvents = eventGroup({
+  source: 'Heroes UI',
+  events: {
+    heroNameChanged: type<string | undefined>(),
+  },
+});
 
 export const HeroesStore = signalStore(
   withState(() => createHeroesView({ hero: { id: 1, name: 'Windstorm' } })),
-  withMethods((state) => ({
-    updateHeroName(heroName: string | undefined): void {
-      const name = heroName?.trim() ?? '';
-
-      patchState(state, {
-        hero: create(state.hero(), (draft) => {
-          draft.name = name;
-        }),
-      });
-    },
-  })),
+  withReducer(
+    on(heroesStateEvents.heroNameChanged, ({ payload: heroName }) =>
+      updateHeroName(heroName),
+    ),
+  ),
 );
+
+function updateHeroName(
+  heroName: string | undefined,
+): PartialStateUpdater<{ hero: Hero }> {
+  const name = heroName?.trim() ?? '';
+
+  return (state) =>
+    create(state, (draft) => {
+      draft.hero.name = name;
+    });
+}
