00282cd2188a0839bde833d806d415926f5f4a41
Jason Warner <xocomil@gmail.com>
2025-07-20 12:01:07 -0600

    refactored to use `HeroesListComponent`

--- libs/ui/src/lib/components/heroes-list.component.ts
+++ libs/ui/src/lib/components/heroes-list.component.ts
@@ -1,15 +1,38 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { HeroesStore } from '../stores/heroes.store';
 
 @Component({
-  selector: 'toh-heores-list',
+  selector: 'toh-heroes-list',
   imports: [CommonModule],
-  template: `<p>heores-list works!</p>`,
-  styles: `
-    :host {
-      display: block;
+  template: `
+    <h2 class="card-title">My Heroes</h2>
+    @for (hero of heroStore.heroes(); track hero.id) {
+      <button
+        class="btn w-60 flex gap-2 ps-0"
+        (click)="selectHero(hero.id)"
+        type="button"
+      >
+        <div
+          class="h-full w-6 bg-primary text-primary-content pe-[2px] flex items-center justify-end"
+        >
+          {{ hero.id }}
+        </div>
+        <div class="flex-grow text-left">{{ hero.name }}</div></button
+      ><br />
     }
   `,
+  host: {
+    class: 'block',
+  },
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeoresListComponent {}
+export class HeroesListComponent {
+  protected readonly heroStore = inject(HeroesStore);
+
+  protected selectHero(id: number) {
+    console.log('hero selected', id);
+
+    this.heroStore.selectHero(id);
+  }
+}
--- libs/ui/src/lib/components/heroes-view.ts
+++ libs/ui/src/lib/components/heroes-view.ts
@@ -1,32 +1,19 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HeroesStore } from '../stores/heroes.store';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesListComponent } from './heroes-list.component';
 
 @Component({
   selector: 'toh-heroes',
-  imports: [CommonModule, HeroDetailComponent],
+  imports: [CommonModule, HeroDetailComponent, HeroesListComponent],
   template: `
     <div
       class="card card-sm shadow-sm bg-neutral not-prose max-h-(--card-height)"
     >
       <div class="card-body flex-row overflow-hidden max-h-full">
         <div class="overflow-y-scroll max-h-full">
-          <h2 class="card-title">My Heroes</h2>
-          @for (hero of heroStore.heroes(); track hero.id) {
-            <button
-              class="btn w-60 flex gap-2 ps-0"
-              (click)="selectHero(hero.id)"
-              type="button"
-            >
-              <div
-                class="h-full w-6 bg-primary text-primary-content pe-[2px] flex items-center justify-end"
-              >
-                {{ hero.id }}
-              </div>
-              <div class="flex-grow text-left">{{ hero.name }}</div></button
-            ><br />
-          }
+          <toh-heroes-list />
         </div>
         <div class="max-h-full prose">
           <toh-hero-detail />
@@ -45,12 +32,4 @@ import { HeroDetailComponent } from './hero-detail.component';
   changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [HeroesStore],
 })
-export class HeroesComponent {
-  protected readonly heroStore = inject(HeroesStore);
-
-  protected selectHero(id: number) {
-    console.log('hero selected', id);
-
-    this.heroStore.selectHero(id);
-  }
-}
+export class HeroesComponent {}
